65e635944d98031a85023949623a8a84abaf34b0
Ananya Dhulipala <ananyad.ei20@rvce.edu.in>
2023-06-22 21:23:26 +0530

    added PIL conversion to the code and comments

--- train_dataset_creation.py
+++ train_dataset_creation.py
@@ -3,14 +3,14 @@ import os
 import re
 from PIL import Image
 
-file_name = os.path.basename('\dataset\train\mild\mild_al1.jpg')
-x = re.search(r"[1-9]",file_name)
+file_name = os.path.basename('\dataset\train\mild\mild_al1.jpg')# opening file name
+x = re.search(r"[1-9]",file_name)# label name 
 label = file_name[:x.span()[0]]
 train_dataset = {}
 train_dataset[label]='\dataset\train\mild\mild_al1.jpg'
-print(train_dataset)
+print(train_dataset)# label name: path
 
-our_file = Path(os.path.abspath('dataset\\train\mild\mild_al1.jpg'))
+our_file = Path(os.path.abspath('dataset\\train\mild\mild_al1.jpg'))# using relative to get absolute path
 img = Image.open(our_file)
 train_dataset[label]=img
 img.close()
