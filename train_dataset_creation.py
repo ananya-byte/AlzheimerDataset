10d1a4c4b18a70f5fca712f2d84ca51d354572f2
Ananya Dhulipala <ananyad.ei20@rvce.edu.in>
2023-06-23 12:17:28 +0530

    stupid changes

--- train_dataset_creation.py
+++ train_dataset_creation.py
@@ -8,10 +8,12 @@ x = re.search(r"[1-9]",file_name)# label name
 label = file_name[:x.span()[0]]
 train_dataset = {}
 
-our_file = Path(os.path.abspath('dataset\\train\mild\mild_al1.jpg'))# using relative to get absolute path
+folder = os.path.abspath('\dataset\train\mild')
+our_file = Path(folder)# using relative to get absolute path
 img = Image.open(our_file)
 img_resized = img.resize((32, 32))
-train_dataset[label]=img_resized
+train_dataset['img']=img_resized
+train_dataset['label']=label
 img.close()
 print(train_dataset)
 
