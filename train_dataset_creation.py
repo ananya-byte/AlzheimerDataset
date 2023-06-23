888ea9645f392a558006ac200c806aecf2fe5588
Nischitha Krishna <nischithakrishna28@gmail.com>
2023-06-23 12:15:37 +0530

    added for loop in the code

--- train_dataset_creation.py
+++ train_dataset_creation.py
@@ -8,10 +8,14 @@ x = re.search(r"[1-9]",file_name)# label name
 label = file_name[:x.span()[0]]
 train_dataset = {}
 
-our_file = Path(os.path.abspath('dataset\\train\mild\mild_al1.jpg'))# using relative to get absolute path
+mild_folder=os.path.abspath('dataset\\train\mild')
+our_file = Path(mild_folder)# using relative to get absolute path
+for file in our_file.iterdir():
+    print(file)
 img = Image.open(our_file)
 img_resized = img.resize((32, 32))
-train_dataset[label]=img_resized
+train_dataset['img']=img_resized
+train_dataset['label']=label
 img.close()
 print(train_dataset)
 
