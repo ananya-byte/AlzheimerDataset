5a42d92867939f3273865fd7bc7aa32d09453ea6
Ananya Dhulipala <ananyad.ei20@rvce.edu.in>
2023-06-23 11:51:53 +0530

    deleted lines that aren't required

--- train_dataset_creation.py
+++ train_dataset_creation.py
@@ -7,13 +7,9 @@ file_name = os.path.basename('\dataset\train\mild\mild_al1.jpg')# opening file n
 x = re.search(r"[1-9]",file_name)# label name 
 label = file_name[:x.span()[0]]
 train_dataset = {}
-train_dataset[label]='\dataset\train\mild\mild_al1.jpg'
-print(train_dataset)# label name: path
 
 our_file = Path(os.path.abspath('dataset\\train\mild\mild_al1.jpg'))# using relative to get absolute path
 img = Image.open(our_file)
-train_dataset[label]=img
-print(f"Original size : {img.size}") # 5464x3640
 img_resized = img.resize((32, 32))
 train_dataset[label]=img_resized
 img.close()
