6bd7cc74fd01b40e33cfee27eec4d58ad2df338d
Nischitha Krishna <nischithakrishna28@gmail.com>
2023-06-23 13:08:45 +0530

    updated code

--- training_dataset.py
+++ training_dataset.py
@@ -3,20 +3,20 @@ import os
 import re
 from PIL import Image
 
-file_name = os.path.basename('\dataset\train\mild\mild_al1.jpg')# opening file name
-x = re.search(r"[1-9]",file_name)# label name 
-label = file_name[:x.span()[0]]
-train_dataset = {}
+train_dataset = []
 
+folder = os.path.abspath('\dataset\train\mild')
+our_file = Path(folder)# using relative to get absolute path
 mild_folder=os.path.abspath('dataset\\train\mild')
-our_file = Path(mild_folder)# using relative to get absolute path
-for file in our_file.iterdir():
-    print(file)
-img = Image.open(our_file)
-img_resized = img.resize((32, 32))
-train_dataset['img']=img_resized
-train_dataset['label']=label
-img.close()
-print(train_dataset)
-
-
+our_folder = Path(mild_folder)# using relative to get absolute path
+for file in our_folder.iterdir():
+    file_name = os.path.basename(file)
+    label = file_name[:re.search(r"[1-9]",file_name).span()[0]]
+    img = Image.open(file)
+    img_resized = img.resize((32, 32))
+    img_dict = {}
+    img_dict['img']=img_resized
+    img_dict['label']=label
+    train_dataset.append(img_dict)
+    img.close()
+print(train_dataset[0]['label'])
