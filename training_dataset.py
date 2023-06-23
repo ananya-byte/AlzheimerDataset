472614b50ca5012a6ad31d1e9528714aa5ec3361
Nischitha Krishna <nischithakrishna28@gmail.com>
2023-06-23 13:27:19 +0530

    deleted README.md

--- training_dataset.py
+++ training_dataset.py
@@ -7,8 +7,8 @@ train_dataset = []
 
 
 mild_folder=os.path.abspath('dataset\\train\mild')
-our_folder = Path(mild_folder)# using relative to get absolute path
-for file in our_folder.iterdir():
+mil_folder = Path(mild_folder)# using relative to get absolute path
+for file in mil_folder.iterdir():
     file_name = os.path.basename(file)
     label = file_name[:re.search(r"[1-9]",file_name).span()[0]]
     img = Image.open(file)
@@ -18,6 +18,48 @@ for file in our_folder.iterdir():
     img_dict['label']=label
     train_dataset.append(img_dict)
     img.close()
-print(train_dataset[0]['label'])
+
+
+moderate_folder=os.path.abspath('dataset\\train\moderate')
+mod_folder = Path(moderate_folder)# using relative to get absolute path
+for file in mod_folder.iterdir():
+    file_name = os.path.basename(file)
+    label = file_name[:re.search(r"[1-9]",file_name).span()[0]]
+    img = Image.open(file)
+    img_resized = img.resize((32, 32))
+    img_dict = {}
+    img_dict['img']=img_resized
+    img_dict['label']=label
+    train_dataset.append(img_dict)
+    img.close()
+
+
+nondemented_folder=os.path.abspath('dataset\\train\nondemented')
+non_folder = Path(nondemented_folder)# using relative to get absolute path
+for file in non_folder.iterdir():
+    file_name = os.path.basename(file)
+    label = file_name[:re.search(r"[1-9]",file_name).span()[0]]
+    img = Image.open(file)
+    img_resized = img.resize((32, 32))
+    img_dict = {}
+    img_dict['img']=img_resized
+    img_dict['label']=label
+    train_dataset.append(img_dict)
+    img.close()
+
+
+verymild_folder=os.path.abspath('dataset\\train\very-mild')
+vm_folder = Path(verymild_folder)# using relative to get absolute path
+for file in vm_folder.iterdir():
+    file_name = os.path.basename(file)
+    label = file_name[:re.search(r"[1-9]",file_name).span()[0]]
+    img = Image.open(file)
+    img_resized = img.resize((32, 32))
+    img_dict = {}
+    img_dict['img']=img_resized
+    img_dict['label']=label
+    train_dataset.append(img_dict)
+    img.close()
+print(train_dataset)
 
 
