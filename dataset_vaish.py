7c3e6cfeb88cf970f7ac1ba86a63a8d83c40957c
unknown <vaishnavimanagoli04@gmail.com>
2023-06-29 22:50:37 +0530

    addede and edited testing code

--- dataset_vaish.py
+++ dataset_vaish.py
@@ -1,27 +1,84 @@
+from pathlib import Path
 import os
+import re
+from PIL import Image
 
-# file name with extension
-file_name = os.path.basename(r'\dataset\test\nondemented\nondemented_al4.jpg')
+test_dataset = []
+labels=['mild_al','moderate_al','verymild_al','nondemented_al']
+n_labels=len(labels)
+n_samples = {}
 
-# file name without extension
-print(os.path.splitext(file_name)[0])
+def  create_testing_dataset():
+    mild_folder=os.path.abspath('dataset\\test\mild')
+    mil_folder = Path(mild_folder)# using relative to get absolute path
+    mild_counter=0
+    for file in mil_folder.iterdir():
+        file_name = os.path.basename(file)
+        label = file_name[:re.search(r"[1-9]",file_name).span()[0]]
+        index=labels.index(label)
+        img = Image.open(file)
+        img_resized = img.resize((224, 224))
+        img_dict = {}
+        img_dict['img']=img_resized
+        img_dict['label']=index
+        test_dataset.append(img_dict)
+        img.close()
+        mild_counter=mild_counter+1
+
+        
+
+    moderate_folder=os.path.abspath('dataset\\test\moderate')
+    mod_folder = Path(moderate_folder)# using relative to get absolute path
+    moderate_counter=0
+    for file in mod_folder.iterdir():
+        file_name = os.path.basename(file)
+        label = file_name[:re.search(r"[1-9]",file_name).span()[0]]
+        index=labels.index(label)
+        img = Image.open(file)
+        img_resized = img.resize((224, 224))
+        img_dict = {}
+        img_dict['img']=img_resized
+        img_dict['label']=index
+        test_dataset.append(img_dict)
+        img.close()
+        moderate_counter=moderate_counter+1
 
 
+    nondemented_folder=os.path.abspath('dataset\\test\\nondemented')
+    non_folder = Path(nondemented_folder)# using relative to get absolute path
+    nondemented_counter=0
+    for file in non_folder.iterdir():
+        file_name = os.path.basename(file)
+        label = file_name[:re.search(r"[1-9]",file_name).span()[0]]
+        index=labels.index(label)
+        img = Image.open(file)
+        img_resized = img.resize((224, 224))
+        img_dict = {}
+        img_dict['img']=img_resized
+        img_dict['label']=index
+        test_dataset.append(img_dict)
+        img.close()
+        nondemented_counter=nondemented_counter+1
 
-# initializing string
-test_str = os.path.basename(r'C:\Users\91974\OneDrive\Desktop\minor\Minor\dataset\test\nondemented\nondemented_al1')
 
-# printing original string
-print(str(test_str))
+    verymild_folder=os.path.abspath('dataset\\test\\verymild')
+    vm_folder = Path(verymild_folder)# using relative to get absolute path
+    verymild_counter=0
+    for file in vm_folder.iterdir():
+        file_name = os.path.basename(file)
+        label = file_name[:re.search(r"[1-9]",file_name).span()[0]]
+        index=labels.index(label)
+        img = Image.open(file)
+        img_resized = img.resize((224, 224))
+        img_dict = {}
+        img_dict['img']=img_resized
+        img_dict['label']=index
+        test_dataset.append(img_dict)
+        img.close()
+        verymild_counter=verymild_counter+1
+        
 
-# loop to iterating characters
-temp = 0
-for chr in test_str:
-	
-# checking if character is numeric,
-# saving index
-    if chr.isdigit():
-	    temp = test_str.index(chr)
-
-# printing result
-print(str(test_str[0 : temp]))
+    n_samples = {0:mild_counter,1:moderate_counter,2:nondemented_counter,3:verymild_counter}
+    print(n_samples)
+    return test_dataset,labels,n_labels,n_samples
+create_testing_dataset()
