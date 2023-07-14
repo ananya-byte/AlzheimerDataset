270a61fccfcac8ce61b99e6899701009b2cf4f0d
unknown <vaishnavimanagoli04@gmail.com>
2023-07-14 23:10:25 +0530

    updated code for creating a tensor testing dataset

--- dataset_vaish.py
+++ dataset_vaish.py
@@ -1,84 +1,73 @@
 from pathlib import Path
 import os
 import re
-from PIL import Image
 
-test_dataset = []
-labels=['mild_al','moderate_al','verymild_al','nondemented_al']
-n_labels=len(labels)
-n_samples = {}
+import tensorflow as tf
+import os
 
-def  create_testing_dataset():
-    mild_folder=os.path.abspath('dataset\\test\mild')
-    mil_folder = Path(mild_folder)# using relative to get absolute path
-    mild_counter=0
-    for file in mil_folder.iterdir():
-        file_name = os.path.basename(file)
-        label = file_name[:re.search(r"[1-9]",file_name).span()[0]]
-        index=labels.index(label)
-        img = Image.open(file)
-        img_resized = img.resize((224, 224)).convert('RGB')
-        img_dict = {}
-        img_dict['img']=img_resized
-        img_dict['label']=index
-        test_dataset.append(img_dict)
-        img.close()
-        mild_counter=mild_counter+1
+# Dataset Parameters - CHANGE HERE
 
+DATASET_PATH = 'dataset\\test' # the dataset file or root folder path.
+
+# Image Parameters
+N_CLASSES = 4 # CHANGE HERE, total number of classes
+IMG_HEIGHT = 224 # CHANGE HERE, the image height to be resized to
+IMG_WIDTH = 224 # CHANGE HERE, the image width to be resized to
+CHANNELS = 3 # The 3 color channels, change to 1 if grayscale
+
+
+
+
+# Reading the dataset
+# 2 modes: 'file' or 'folder'
+def read_images(dataset_path,batch_size):
+    imagepaths, labels = list(), list()
+   
         
+        # An ID will be affected to each sub-folders by alphabetical order
+    label = 0
+        # List the directory
+    try:  # Python 2
+        classes = sorted(os.walk(dataset_path).next()[1])
+    except Exception:  # Python 3
+        classes = sorted(os.walk(dataset_path).__next__()[1])
+        # List each sub-directory (the classes)
+    for c in classes:
+        c_dir = os.path.join(dataset_path, c)
+        try:  # Python 2
+            walk = os.walk(c_dir).next()
+        except Exception:  # Python 3
+            walk = os.walk(c_dir).__next__()
+            # Add each image to the training set
+        for sample in walk[2]:
+                # Only keeps jpeg images
+            if sample.endswith('.jpg') or sample.endswith('.jpeg'):
+                    imagepaths.append(os.path.join(c_dir, sample))
+                    labels.append(label)
+            label += 1
+    else:
+        raise Exception("Unknown mode.")
 
-    moderate_folder=os.path.abspath('dataset\\test\moderate')
-    mod_folder = Path(moderate_folder)# using relative to get absolute path
-    moderate_counter=0
-    for file in mod_folder.iterdir():
-        file_name = os.path.basename(file)
-        label = file_name[:re.search(r"[1-9]",file_name).span()[0]]
-        index=labels.index(label)
-        img = Image.open(file)
-        img_resized = img.resize((224, 224)).convert('RGB')
-        img_dict = {}
-        img_dict['img']=img_resized
-        img_dict['label']=index
-        test_dataset.append(img_dict)
-        img.close()
-        moderate_counter=moderate_counter+1
+    # Convert to Tensor
+    imagepaths = tf.convert_to_tensor(imagepaths, dtype=tf.string)
+    labels = tf.convert_to_tensor(labels, dtype=tf.int32)
+    # Build a TF Queue, shuffle data
+    image, label = tf.train.slice_input_producer([imagepaths, labels],
+                                                 shuffle=True)
 
+    # Read images from disk
+    image = tf.read_file(image)
+    image = tf.image.decode_jpeg(image, channels=CHANNELS)
 
-    nondemented_folder=os.path.abspath('dataset\\test\\nondemented')
-    non_folder = Path(nondemented_folder)# using relative to get absolute path
-    nondemented_counter=0
-    for file in non_folder.iterdir():
-        file_name = os.path.basename(file)
-        label = file_name[:re.search(r"[1-9]",file_name).span()[0]]
-        index=labels.index(label)
-        img = Image.open(file)
-        img_resized = img.resize((224, 224)).convert('RGB')
-        img_dict = {}
-        img_dict['img']=img_resized
-        img_dict['label']=index
-        test_dataset.append(img_dict)
-        img.close()
-        nondemented_counter=nondemented_counter+1
+    # Resize images to a common size
+    image = tf.image.resize_images(image, [IMG_HEIGHT, IMG_WIDTH])
 
+    # Normalize
+    image = image * 1.0/127.5 - 1.0
 
-    verymild_folder=os.path.abspath('dataset\\test\\verymild')
-    vm_folder = Path(verymild_folder)# using relative to get absolute path
-    verymild_counter=0
-    for file in vm_folder.iterdir():
-        file_name = os.path.basename(file)
-        label = file_name[:re.search(r"[1-9]",file_name).span()[0]]
-        index=labels.index(label)
-        img = Image.open(file)
-        img_resized = img.resize((224, 224)).convert('RGB')
-        img_dict = {}
-        img_dict['img']=img_resized
-        img_dict['label']=index
-        test_dataset.append(img_dict)
-        img.close()
-        verymild_counter=verymild_counter+1
-        
+    # Create batches
+    X, Y = tf.train.batch([image, label], batch_size=batch_size,
+                          capacity=batch_size * 8,
+                          num_threads=4)
 
-    n_samples = {0:mild_counter,1:moderate_counter,2:nondemented_counter,3:verymild_counter}
-
-    return test_dataset,labels,n_labels,n_samples
-create_testing_dataset()
+    return X, Y
