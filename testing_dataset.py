9cbdb2df227f7b683b5d436ba7897a3399944487
unknown <vaishnavimanagoli04@gmail.com>
2023-07-16 13:45:04 +0530

    edited code to hugging face loading of custom dataset

---
+++ testing_dataset.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+import os
+from datasets import load_dataset
+
+
+# Dataset Parameters - CHANGE HERE
+DATASET_PATH = 'dataset\\test' # the dataset file or root folder path.
+
+# Image Parameters
+n_classes = 4 # CHANGE HERE, total number of classes
+img_height = 224# CHANGE HERE, the image height to be resized to
+img_width = 224 # CHANGE HERE, the image width to be resized to
+channels = 3 
+batch_size= 130
+
+dataset = load_dataset("imagefolder", data_dir=DATASET_PATH,drop_labels=False)             
+def transforms(examples):
+    examples["pixel_values"] = [image.convert("RGB").resize((224,224)) for image in examples["image"]]
+    return examples
+dataset = dataset.map(transforms, remove_columns=["image"], batched=True)
+print(dataset)
