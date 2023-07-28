fe6c77614729d0e2cb5901e8a4fb09f1bb2d5f13
Ananya Dhulipala <ananyad.ei20@rvce.edu.in>
2023-07-28 11:35:56 +0530

    edited final

--- final.py
+++ final.py
@@ -45,11 +45,8 @@ cfm_metric = evaluate.load("BucketHeadP65/confusion_matrix")
 n= 0
 metric = load_metric("accuracy")
 def compute_metrics(p):
-    global n
-    n = n+1
-    if (n>11):
-        results = cfm_metric.compute(references=p.label_ids, predictions=np.argmax(p.predictions, axis=1))
-        print(results)
+    results = cfm_metric.compute(references=p.label_ids, predictions=np.argmax(p.predictions, axis=1))
+    print(results)
     return metric.compute(
         predictions=np.argmax(p.predictions, axis=1),
         references=p.label_ids
